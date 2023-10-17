62f68b7b20c84fa51ce64b04d2a68180c675877f
lsioctl <laurent.steff@gmail.com>
2023-10-17 18:27:43 +0200

    minimal rust binary

---
+++ README.md
@@ -0,0 +1,9 @@
+# embedded-rust-os
+
+This is a personal toy project to continue learning Rust and having fun with embedded and operating system
+
+All the ideas, and code comes from this awesome blog:
+
+https://os.phil-opp.com/
+
+I may add some comments to remember things in the code and change slightly some minor details
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+// we don't want to use the normal entry point chain
+// normally for rust linked with std lib
+// * execution starts in a C runtime library (crt0)
+// which prepares a C application (setting the stack,
+// arguments in the right register)
+// * then crt0 invoke entry point of the Rust runtime
+// marked by the `start` language item
+// Rust runtime is very minimal (stack overflow guards,
+// printing backtrace on panic, ...)
+// * Rust runtime then calls the main function
+// note than when overwriting crt0 entry point (_start function)
+// the linker will complain because it will assume that we
+// are running on C runtime
+// So we will have to had a custom target triple with none as the os
+// like: thumbv7em-none-eabihf
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+/// with no_std we have to implement our own
+#[panic_handler]
+// should never return so marked as diverging function
+// with the return type ! (never)
+// PanicInfo containes the file and the line where the
+// panic happened, and optional message
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// overwriting the crt0 entry point
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
